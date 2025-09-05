1062d7002f2e35becf017c777165768833472c68
jeevana4587 <jeevanapavuluru4587@gmail.com>
2025-09-05 23:01:13 +0530

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
